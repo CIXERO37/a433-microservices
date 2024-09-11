9bb9d54535a84c10286e0c2376e82a5557c77fe4
CIXERO37 <dlol59005@gmail.com>
2024-09-11 13:23:55 +0700

    auto

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t item-app:1 .
 docker images
-docker tag item-app:1 ghcr.io/cixero37/item-app:v1
-docker login ghcr.io
-docker push ghcr.io/cixero37/item-app:v1
+docker tag item-app:1 cixero37/item-app:v1
+docker login
+docker push cixero37/item-app:v1
