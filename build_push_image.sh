19ab250d2e20eec83c9fbdb105cbe5067b898b3c
CIXERO37 <dlol59005@gmail.com>
2024-09-12 08:34:28 +0700

    auto-lgn

--- build_push_image.sh
+++ build_push_image.sh
@@ -2,5 +2,5 @@
 docker build -t item-app:1 .
 docker images
 docker tag item-app:1 cixero37/item-app:v1
-docker login
+echo $PASS | docker login -u cixero37 --password-stdin
 docker push cixero37/item-app:v1
